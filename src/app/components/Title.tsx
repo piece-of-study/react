50cdd6fd2e91a53af99c972bab8c0ce5ab5324a8
jojo <jojo00923@gmail.com>
2024-03-28 23:15:13 +0900

    title.tsx 파일 주석 추가

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -4,7 +4,9 @@ interface Props {
   value?: string;
   onInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-
+// export default 이걸  기본으로 export하겠다.
 export default function Title({value, onInput}: Props) {
+  // value라는 변수와, onInput이라는 변수의 타입은 Props 인터페이스에 설정되어있음
   return <input type="text" value={value} onInput={onInput} />;
+  // 사싣 함수를 호출하면 input 태그를 리턴함. 값은 value임. 입력값은 OnInput임
 }

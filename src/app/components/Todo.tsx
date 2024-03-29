e9cd3dbc1cc0132b035e0b26e9c216d1fe5b9b7f
jojo <jojo00923@gmail.com>
2024-03-29 23:57:54 +0900

    Todi.tsx 주석 추가

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// 변수들의 타입 명시
 type Props = {
   value?: string;
   onInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -13,12 +14,13 @@ function Todo({value, onInput, onKeyDown}: Props) {
       <input
         className="todo-input"
         type="text"
-        value={value}
-        onInput={onInput}
-        onKeyDown={onKeyDown}
+        value={value} // 값
+        onInput={onInput} // 입력창에 뭘 입력하면 함수
+        onKeyDown={onKeyDown} // 키 누를때 함수
       />
     </div>
   );
 }
 
+// 다른곳에서 임포트하기 위해 사용하는데 export야.
 export default Todo;
